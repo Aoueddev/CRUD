53b0bd898c03b07686e23630dbc01b814baae6df
Aoued Mekki <aouedmekki1@gmail.com>
2024-12-26 16:07:35 -0300

    Feat: Routes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", function(){
   return redirect()-> route("produtos.index");
 });
-Route::resource('produtos', ProdutoController::class);
+//Route::resource('produtos', ProdutoController::class);
+Route::get('produtos', [ProdutoController::class, 'index'])->name('produtos.index');
 
+Route::get('produtos/create', [ProdutoController::class, 'create'])->name('produtos.create');
+
+
+Route::post('produtos', [ProdutoController::class, 'store'])->name('produtos.store');
+
+
+Route::get('produtos/{produto}', [ProdutoController::class, 'show'])->name('produtos.show');
+
+Route::get('produtos/{produto}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
+
+
+Route::put('produtos/{produto}', [ProdutoController::class, 'update'])->name('produtos.update');
+
+
+Route::delete('produtos/{produto}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
