0117275fe7581c20818636866c4a1c9cc151fd50
Aoued Mekki <aoued.mekki@bomix01.dns>
2024-12-26 09:19:11 -0300

    Feat: ajustes

---
+++ resources/views/_components/form_create.blade.php
@@ -0,0 +1,39 @@
+
+  <form action="{{ route('produtos.store') }}" method="POST">
+        @csrf
+       <div class="form-group">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" value="{{ old('nome') }}">
+            @error('nome')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="descricao">Descrição:</label>
+            <textarea id="descricao" name="descricao">{{ old('descricao') }}</textarea>
+            @error('descricao')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="preco">Preço:</label>
+            <input type="text" id="preco" name="preco" value="{{ old('preco') }}">
+            @error('preco')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="estoque">Estoque:</label>
+            <input type="number" id="estoque" name="estoque" value="{{ old('estoque') }}">
+            @error('estoque')
+                <div class="error-message">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <button type="submit">
+            <i class="fas fa-plus-circle"></i> Criar Produto
+        </button>
+    </form>
