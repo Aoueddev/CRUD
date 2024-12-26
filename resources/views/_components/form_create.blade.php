b49198be31fab5216836bfdebb3d6c22183ef62e
Aoued Mekki <aoued.mekki@bomix01.dns>
2024-12-26 10:23:48 -0300

    Feat: Validação no cadastro

--- resources/views/_components/form_create.blade.php
+++ resources/views/_components/form_create.blade.php
@@ -1,12 +1,21 @@
+
 <form action="{{ route('produtos.store') }}" method="POST">
         @csrf
-       <div class="form-group">
+        <div class="form-group">
             <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" value="{{ old('nome') }}">
+            <input 
+                type="text" 
+                id="nome" 
+                name="nome" 
+                value="{{ old('nome') }}" 
+                required 
+                maxlength="255"
+                title="O nome é obrigatório e não pode ser vazio.">
             @error('nome')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
+        
 
         <div class="form-group">
             <label for="descricao">Descrição:</label>
@@ -18,15 +27,22 @@
 
         <div class="form-group">
             <label for="preco">Preço:</label>
-            <input type="text" id="preco" name="preco" value="{{ old('preco') }}">
+            <input 
+                type="text" 
+                id="preco" 
+                name="preco" 
+                value="{{ old('preco') }}" 
+                pattern="^\d+(\.\d{1,2})?$" 
+                title="Insira um valor numérico válido. Exemplo: 10 ou 10.99"
+                required>
             @error('preco')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
-
+        
         <div class="form-group">
             <label for="estoque">Estoque:</label>
-            <input type="number" id="estoque" name="estoque" value="{{ old('estoque') }}">
+            <input type="number" id="estoque"  required name="estoque" value="{{ old('estoque') }}">
             @error('estoque')
                 <div class="error-message">{{ $message }}</div>
             @enderror
